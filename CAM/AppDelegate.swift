70bcde9eb4ba9cc7b6dd44104ce27108decda404
Gabriel Rinaldi <gabriel_rinaldi@mac.com>
2019-01-19 15:51:05 -0800

    Initial Commit

---
+++ CAM/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CAM
+//
+//  Created by Gabriel Rinaldi on 1/19/19.
+//  Copyright © 2019 Gabriel Rinaldi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CAM/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CAM
+//
+//  Created by Gabriel Rinaldi on 1/19/19.
+//  Copyright © 2019 Gabriel Rinaldi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
